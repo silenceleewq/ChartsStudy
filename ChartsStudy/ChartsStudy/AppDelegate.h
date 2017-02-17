4c18ae85177b5d45316811a6cff6fbfa446db875
LZY <958246321@qq.com>
2017-02-17 08:32:38 +0800

    初始化项目

---
+++ ChartsStudy/ChartsStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChartsStudy
+//
+//  Created by slience on 2017/2/17.
+//  Copyright © 2017年 Ninja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
